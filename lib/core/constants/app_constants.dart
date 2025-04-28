a8d6eac44ddea9d92fefd6be94539fde9a831935
mahmoudmohamed17 <ma5975986@gmail.com>
2025-04-28 18:11:51 +0300

    Setup app logo, splash, fonts and initilize the app.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -6,3 +6,6 @@ const FontWeight semiBold = FontWeight.w600;
 const FontWeight medium = FontWeight.w500;
 const FontWeight regular = FontWeight.w400;
 const FontWeight light = FontWeight.w300;
+
+// Font family constant
+const String fontFamily = 'Poppins';
--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -1,33 +1,112 @@
 import 'package:chat_bot_app/core/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 
+
 class AppTextStyles {
   // Bold
-  static const TextStyle bold28 = TextStyle(fontSize: 28, fontWeight: bold);
-  static const TextStyle bold24 = TextStyle(fontSize: 24, fontWeight: bold);
-  static const TextStyle bold20 = TextStyle(fontSize: 20, fontWeight: bold);
-  static const TextStyle bold18 = TextStyle(fontSize: 18, fontWeight: bold);
-  static const TextStyle bold16 = TextStyle(fontSize: 16, fontWeight: bold);
-  
+  static const TextStyle bold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: bold,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle bold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: bold,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle bold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: bold,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle bold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: bold,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle bold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: bold,
+    fontFamily: fontFamily,
+  );
+
   // SemiBold
-  static const TextStyle semiBold20 = TextStyle(fontSize: 20, fontWeight: semiBold);
-  static const TextStyle semiBold18 = TextStyle(fontSize: 18, fontWeight: semiBold);
-  static const TextStyle semiBold16 = TextStyle(fontSize: 16, fontWeight: semiBold);
-  static const TextStyle semiBold14 = TextStyle(fontSize: 14, fontWeight: semiBold);
-  
+  static const TextStyle semiBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: semiBold,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle semiBold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: semiBold,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle semiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: semiBold,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle semiBold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: semiBold,
+    fontFamily: fontFamily,
+  );
+
   // Medium
-  static const TextStyle medium16 = TextStyle(fontSize: 16, fontWeight: medium);
-  static const TextStyle medium14 = TextStyle(fontSize: 14, fontWeight: medium);
-  static const TextStyle medium12 = TextStyle(fontSize: 12, fontWeight: medium);
-  
+  static const TextStyle medium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: medium,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle medium14 = TextStyle(
+    fontSize: 14,
+    fontWeight: medium,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle medium12 = TextStyle(
+    fontSize: 12,
+    fontWeight: medium,
+    fontFamily: fontFamily,
+  );
+
   // Regular
-  static const TextStyle regular18 = TextStyle(fontSize: 18, fontWeight: regular);
-  static const TextStyle regular16 = TextStyle(fontSize: 16, fontWeight: regular);
-  static const TextStyle regular14 = TextStyle(fontSize: 14, fontWeight: regular);
-  static const TextStyle regular12 = TextStyle(fontSize: 12, fontWeight: regular);
-  
+  static const TextStyle regular18 = TextStyle(
+    fontSize: 18,
+    fontWeight: regular,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle regular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: regular,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle regular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: regular,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle regular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: regular,
+    fontFamily: fontFamily,
+  );
+
   // Light
-  static const TextStyle light16 = TextStyle(fontSize: 16, fontWeight: light);
-  static const TextStyle light14 = TextStyle(fontSize: 14, fontWeight: light);
-  static const TextStyle light12 = TextStyle(fontSize: 12, fontWeight: light);
+  static const TextStyle light16 = TextStyle(
+    fontSize: 16,
+    fontWeight: light,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle light14 = TextStyle(
+    fontSize: 14,
+    fontWeight: light,
+    fontFamily: fontFamily,
+  );
+  static const TextStyle light12 = TextStyle(
+    fontSize: 12,
+    fontWeight: light,
+    fontFamily: fontFamily,
+  );
 }
+
+// Bold
