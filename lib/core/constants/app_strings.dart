bdd09876fbf2a873dbba80e2eb81b5e160638949
mahmoudmohamed17 <ma5975986@gmail.com>
2025-04-29 08:05:12 +0300

    Add app strings and privacy policy for GemiChat application

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,127 @@
+class AppStrings {
+  static const String appName = 'GemiChat';
+  static const String onboardingText1 = 'Special, smart and everything';
+  static const String onboardingText2 = 'Got a problem? Talk to GemiChat...';
+  static const String onboardingText3 = 'Don\'t worry. Your account is safe';
+  static const String onboardingSubtext1 =
+      'GemiChat bot will become your smartest friend during this period.';
+  static const String onboardingSubtext2 =
+      'GemiChat bot has all your needs covered with various features.';
+  static const String onboardingSubtext3 =
+      'Your safety is ours. But for this you need to set a good password.';
+
+  static const String next = 'Next';
+  static const String skip = 'Skip';
+  static const String confirm = 'Confirm';
+  static const String textContinue = 'Continue';
+  static const String edit = 'Edit';
+  static const String delete = 'Delete';
+  static const String close = 'Close';
+  static const String save = 'Save';
+  static const String signIn = 'Sign in';
+  static const String welcome = 'Welcome';
+  static const String signInHint =
+      'Please enter your email and password to sign in.';
+  static const String emailAddress = 'Email address';
+  static const String password = 'Password';
+  static const String forgotPassword = 'Forgot password?';
+  static const String rememberMe = 'Remember me';
+  static const String continueWithGoogle = 'Continue with Google';
+  static const String continueWithApple = 'Continue with Apple';
+  static const String hadVeryLittle = 'Had very little!';
+  static const String redirectToHomePageMessage =
+      'You will be redirected to home page shortly.';
+  static const String resetPassword = 'Reset password';
+  static const String resetPasswordHint =
+      'To reset your password you will need the next clear OTP code sent to your email.';
+  static const String otpCodeVerification = 'OTP code verification';
+  static const String otpCodeVerificationHint =
+      'Please check your email address and write the OTP code you received here.';
+  static const String dontReceiveMail = 'Don\'t receive mail?';
+  static String resendCode(int seconds) => 'You can resend code in $seconds s';
+  static const String createNewPassword = 'Create new password';
+  static const String createNewPasswordHint =
+      'Your password should be difficult and not known to other people.';
+  static const String createAccount = 'Create account';
+  static const String createAccountHint =
+      'Please enter your email and password to sign up.';
+  static const String confirmToPolicy =
+      'I agree to GemiChat bo Terms & Policy.';
+  static const String yourGender = 'Your gender';
+  static const String yourGenderHint = 'What\'s your gender?';
+  static const String male = 'Male';
+  static const String female = 'Female';
+  static const String other = 'Other';
+  static const String personalInfo = 'Personal information';
+  static const String fullName = 'Full name';
+  static const String phoneNumber = 'Phone number';
+  static const String dateOfBirth = 'Date of birth';
+  static const String setYourPINCode = 'Set your PIN code';
+  static const String setYourPINCodeHint =
+      'Add a PIN to make your account more secure.';
+  static const String youAreReady = 'You are ready!';
+  static const String youAreReadyHint =
+      'Your account is ready. Let\'s start for your better chatting exprience.';
+  static const String letsChatting = 'Let\'s chatting!';
+  static const String startChat = 'Start chat';
+  static const String startChatHint =
+      'You should start asking us everything that interests you!';
+  static const String chat = 'Chat';
+  static const String myTopics = 'My topics';
+  static const String myProfile = 'My profile';
+  static const String history = 'History';
+  static const String recent = 'Recent';
+  static const String areYouSureToDelete = 'Are you sure to delete?';
+  static const String areYouSureToDeleteHint =
+      'If you delete it all your correspondence will be permanently deleted and cannot be recovered.';
+  static const String historyIsEmpty = 'History is empty!';
+  static const String historyIsEmptyHint =
+      'Start a new correspondence and send us questions.';
+  static const String lightMode = 'Light mode';
+  static const String darkMode = 'Dark mode';
+  static const String aboutGemiChatBot = 'About GemiChat bot';
+  static const String privacyPolicy = 'Privacy policy';
+  static const String logout = 'Log out';
+  static const String english = 'English (EN)';
+  static const String arabic = 'Arabic (AR)';
+  static const String privacyPolicyText =
+      '''At GemiChat, we are committed to protecting your privacy and ensuring transparency in how we handle your personal information.
+      This Privacy Policy explains what data we collect, how we use it, and the measures we take to protect it.\n
+      1. Information Collection\n
+      We may collect limited information, including but not limited to device details, usage statistics, and conversation data. This data is collected solely to enhance the functionality and improve the user experience of the GemiChat application.\n
+      2. Use of Information\n
+      Collected information is used for the following purposes:\n
+      Improving service functionality and performance.\n
+      Personalizing user experience.\n
+      Troubleshooting technical issues.\n
+      Communicating updates and important notifications related to the application.\n
+      We do not sell, rent, or share your personal data with third parties without your explicit consent, except as required by applicable law.\n
+      3. Data Security\n
+      We implement appropriate technical and organizational measures to safeguard your personal information from unauthorized access, alteration, disclosure, or destruction.
+      While we strive to use commercially acceptable means to protect your personal data, no method of transmission over the Internet or method of electronic storage is 100% secure.\n
+      4. User Responsibilities\n
+      By using GemiChat, you agree to:\n
+      Use the application in compliance with all applicable laws and regulations.\n
+      Avoid using the application for unlawful, harmful, or abusive activities.\n
+      Maintain the confidentiality of any credentials or sensitive information associated with your use of the application.\n
+      You are solely responsible for any activity conducted through your account or device.\n
+      5. Third-Party Services\n
+      GemiChat may include links to or integrations with third-party websites, applications, or services that are not operated by us.\n
+      We are not responsible for the content, privacy policies, or practices of any third-party services.\n
+      We recommend that you review the privacy policies of any third-party services you interact with through GemiChat.\n
+      6. Changes to This Policy\n
+      We reserve the right to update or modify this Privacy Policy at any time.\n
+      When changes are made, we will update the "Effective Date" at the top of this policy and may provide additional notice as necessary within the application.\n
+      By continuing to use GemiChat after any changes become effective, you agree to be bound by the revised Privacy Policy.\n
+      7. Contact Us\n
+      If you have any questions, concerns, or feedback regarding this Privacy Policy or GemiChat’s data practices, please contact us at:\n
+      Email: ma5975986@gmail.com
+        ''';
+  static const String aboutGemiChatBotText =
+      '''GemiChat is an intelligent conversational assistant designed to offer users a friendly, supportive, and efficient communication experience.\n
+      Leveraging advanced language models and smart algorithms, GemiChat assists users with answering questions, offering recommendations, casual chatting, and boosting daily productivity.\n
+      Our mission is to provide a seamless, natural, and highly personalized interaction that meets the communication needs of modern users.\n
+      We are constantly working to enhance GemiChat’s capabilities to ensure it remains a reliable, innovative, and trusted companion in your digital life.
+      Thank you for choosing GemiChat — your intelligent partner for smarter conversations.
+        ''';
+}
